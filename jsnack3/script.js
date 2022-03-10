93f0f29dc9f28ea3559a234c3ecc9983d53675c6
Mario Rovito <alessandro.r14@gmail.com>
2022-03-10 18:32:51 +0100

    add prompt and while for addiction

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -2,10 +2,16 @@ console.log('JS OK!')
 
 // Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero. 
 
-
-// Chiedi un numero di 4 cifre all'utente
+//chiedi un numero di 4 cifre
 var numero = parseInt( prompt('Inserisci un numero di 4 cifre!'));
-if(numero.length !== 4){
-    alert('Inserisci un numero di 4 cifre!')
+
+
+//calcola la somma di tutte le cifre che compongono il numero. 
+somma = 0;
+
+while (numero) {
+    somma += numero % 10;
+    numero = Math.floor(numero / 10);
 }
- 
+
+console.log(somma);
