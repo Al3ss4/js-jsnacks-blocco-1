c44d95e17fcf82207ee37917c152684a697525ef
Mario Rovito <alessandro.r14@gmail.com>
2022-03-10 17:36:44 +0100

    add prompt

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,1 +1,11 @@
 console.log('JS OK!')
+
+// Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero. 
+
+
+// Chiedi un numero di 4 cifre all'utente
+var numero = parseInt( prompt('Inserisci un numero di 4 cifre!'));
+if(numero.length !== 4){
+    alert('Inserisci un numero di 4 cifre!')
+}
+ 
