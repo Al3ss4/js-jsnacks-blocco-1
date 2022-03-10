f8cd5fed51d22c935dab71d7bc5decef8edee306
Mario Rovito <alessandro.r14@gmail.com>
2022-03-10 16:09:47 +0100

    add array and prompt

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,1 +1,13 @@
 console.log('JS OK!')
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+
+// creazione array invitati
+
+const listaInvitati = ['Luca', 'Giovanni', 'Paolo', 'Michele', 'Luigi', 'Mario', 'Alessandro', 'Francesco', 'Ivan', 'Mattia'];
+
+// richiesta nome partecipante 
+const nomePartecipante = prompt('Qual è il tuo nome?');
+
+console.log(nomePartecipante);
