b47281dc0b3045beed391a2d67f2673034ad72ab
Mario Rovito <alessandro.r14@gmail.com>
2022-03-10 16:58:16 +0100

    add  for and if

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,1 +1,19 @@
 console.log('JS OK!')
+
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
+
+//array vuoto
+
+array=[]
+
+//Chiedi per 6 volte all’utente di inserire un numero
+
+for(var i=0 ; i<6 ; i++){
+    numero= parseInt(prompt('inserisci un numero'));
+    console.log(numero);
+
+//se è dispari inseriscilo nell’array
+    if( numero % 2 ==! 0){
+        array.push(numero)
+    }
+}
